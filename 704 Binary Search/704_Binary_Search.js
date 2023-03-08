0fb096d61bf6bd02f81c619045fd1f3c281540f0
Joshua Lehman <joshlehman.dev@gmail.com>
2023-03-08 12:15:11 -0500

    commit

--- 704 Binary Search/704_Binary_Search.js
+++ 704 Binary Search/704_Binary_Search.js
@@ -9,8 +9,24 @@ const nums = [-1, 0, 3, 5, 9, 12];
 
 const target = 4;
 
-function Search(arr, val) {
-  
+function Search(nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (target < nums[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
 }
 
 const test = Search(nums, target);
