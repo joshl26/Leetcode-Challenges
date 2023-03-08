d51699d88bd0de8676572171051b896faf3faf6a
Joshua Lehman <joshlehman.dev@gmail.com>
2023-03-08 13:29:38 -0500

    commit

--- 278 First Bad Version/278_First_Bad_Version.js
+++ 278 First Bad Version/278_First_Bad_Version.js
@@ -1,5 +1,5 @@
 //  Definition for isBadVersion()
-  
+
 //  @param {integer} version number
 //  @return {boolean} whether the version is bad
 //  isBadVersion = function(version) {
@@ -8,13 +8,26 @@
 
 //  @param {function} isBadVersion()
 //  @return {function}
- 
-var solution = function(isBadVersion) {
-    /**
-     * @param {integer} n Total versions
-     * @return {integer} The first bad version
-     */
-    return function(n) {
-        
-    };
+
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let start = 0;
+    let end = n;
+
+    while (start <= end) {
+      let mid = Math.floor(start + (end - start) / 2);
+
+      if (isBadVersion(mid) === true && isBadVersion(mid - 1) == false) {
+        return mid;
+      } else if (isBadVersion(mid) === false) {
+        start = mid + 1;
+      } else {
+        end = mid;
+      }
+    }
+  };
 };
