4c1476e4c48536bfa6a96fd5a7a4f4556aa50c1e
Joshua Lehman <joshlehman.dev@gmail.com>
2023-03-12 22:06:41 -0400

    commit

--- 977_squares_of_a_sorted_array/977_squares_of_a_sorted_array.js
+++ 977_squares_of_a_sorted_array/977_squares_of_a_sorted_array.js
@@ -2,6 +2,21 @@
 //  * @param {number[]} nums
 //  * @return {number[]}
 //  */ -->
-var sortedSquares = function(nums) {
-    
+var sortedSquares = function (nums) {
+  let res = [nums.length];
+
+  let start = 0;
+  let end = nums.length - 1;
+  let resIndex = nums.length - 1;
+
+  while (start <= end) {
+    if (nums[start] * nums[start] > nums[end] * nums[end]) {
+      res[resIndex--] = nums[start] * nums[start];
+      start++;
+    } else {
+      res[resIndex--] = nums[end] * nums[end];
+      end--;
+    }
+  }
+  return res;
 };
