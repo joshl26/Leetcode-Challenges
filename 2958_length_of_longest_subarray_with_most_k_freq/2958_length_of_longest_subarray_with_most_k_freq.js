7553b1e922d858d426c425b49e970d4630a54698
Joshua Lehman <joshlehman.dev@gmail.com>
2024-03-28 12:37:33 -0400

    updates

--- 2958_length_of_longest_subarray_with_most_k_freq/2958_length_of_longest_subarray_with_most_k_freq.js
+++ 2958_length_of_longest_subarray_with_most_k_freq/2958_length_of_longest_subarray_with_most_k_freq.js
@@ -1,8 +1,28 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
-var maxSubarrayLength = function(nums, k) {
-    
-};
+function maxSubarrayLength(nums, k) {
+  let frequency = {};
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    let num = nums[right];
+    frequency[num] = (frequency[num] || 0) + 1;
+
+    while (Math.max(...Object.values(frequency)) > k) {
+      let leftNum = nums[left];
+      frequency[leftNum]--;
+      if (frequency[leftNum] === 0) {
+        delete frequency[leftNum];
+      }
+      left++;
+    }
+
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
+// Example usage:
+let nums = [1, 2, 3, 1, 2, 3, 1, 2];
+let k = 2;
+console.log(maxSubarrayLength(nums, k)); // Output: 4 (The longest subarray with at most 2 frequency is [1, 2, 1, 2])
--- 344_Reverse_String/344_Reverse_String.js
+++ 344_Reverse_String/344_Reverse_String.js
@@ -3,18 +3,27 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 
-var reverseString = function(s) {
+//ALTERNATE SOLUTION
+//apply two pointer method where nums: string[]
+//   var left = 0;
+//   for (var right = nums.length - 1; right >= left; right--) {
+//     var temp = nums[left]; // declaring a variable to memorize left value before it is changed
+//     nums[left] = nums[right];
+//     nums[right] = temp;
+//     left++;
+//   }
+//   return nums;
 
-    let l = s.length;
-    
-    let ch = '';
-    
-    for(let i=0; i < l / 2; i++ ){
-        ch = s[i];
-    
-        s[i] = s[l-(i+1)];
-    
-        s[l-(i+1)] = ch;
-    }
-    
-    };
+var reverseString = function (s) {
+  let l = s.length;
+
+  let ch = "";
+
+  for (let i = 0; i < l / 2; i++) {
+    ch = s[i];
+
+    s[i] = s[l - (i + 1)];
+
+    s[l - (i + 1)] = ch;
+  }
+};
